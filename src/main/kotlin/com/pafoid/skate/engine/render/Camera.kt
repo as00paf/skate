eebe6ac3b463dead4f66cfa97c2b126868b7263b
Alexandre Fournier <as00paf@gmail.com>
2026-01-24 01:05:46 -0500

    feat: refactor Camera to support orthographic projection

--- src/main/kotlin/com/pafoid/skate/engine/render/Camera.kt
+++ src/main/kotlin/com/pafoid/skate/engine/render/Camera.kt
@@ -3,52 +3,63 @@ package com.pafoid.skate.engine.render
 import com.pafoid.skate.engine.controls.KeyListener
 import com.pafoid.skate.engine.utils.toRadians
 import org.joml.Matrix4f
+import org.joml.Vector2f
 import org.joml.Vector3f
 import org.lwjgl.glfw.GLFW.*
-import kotlin.math.tan
-
-
-const val FOV = 135f
-const val NEAR_PLANE = 0.1f
-const val FAR_PLANE = 1000f
 
 class Camera(
     val position: Vector3f = Vector3f(),
     var pitch: Float = 0f,
     var yaw: Float = 0f,
-    var roll: Float = 0f
+    var roll: Float = 0f,
+    var isOrthographic: Boolean = false
 ) {
+    var fov = 45f
+    var nearPlane = 0.1f
+    var farPlane = 1000f
+    
+    // Orthographic specific
+    var projectionSize = Vector2f(32f, 18f) // Default 16:9 units
 
     fun move() {
+        val speed = 0.1f
         if (KeyListener.isKeyPressed(GLFW_KEY_W)) {
-            position.z -= 0.02f
+            position.z -= speed
         }
         if (KeyListener.isKeyPressed(GLFW_KEY_S)) {
-            position.z += 0.02f
+            position.z += speed
         }
         if (KeyListener.isKeyPressed(GLFW_KEY_D)) {
-            position.x += 0.02f
+            position.x += speed
         }
         if (KeyListener.isKeyPressed(GLFW_KEY_A)) {
-            position.x -= 0.02f
+            position.x -= speed
+        }
+        if (KeyListener.isKeyPressed(GLFW_KEY_E)) {
+            position.y += speed
+        }
+        if (KeyListener.isKeyPressed(GLFW_KEY_Q)) {
+            position.y -= speed
         }
     }
 
     fun createProjectionMatrix(): Matrix4f {
-        val screenWidth = 1854//1920
-        val screenHeight = 1057//1080
-        val aspectRatio = screenWidth.toFloat() / screenHeight.toFloat()
-        val yScale = (1f / tan(Math.toRadians((FOV / 2f).toDouble())) * aspectRatio).toFloat()
-        val xScale = yScale / aspectRatio
-        val viewDistance = FAR_PLANE - NEAR_PLANE
-
         val projectionMatrix = Matrix4f()
-        projectionMatrix.m00(xScale)
-        projectionMatrix.m11(yScale)
-        projectionMatrix.m22(-((FAR_PLANE + NEAR_PLANE) / viewDistance))
-        projectionMatrix.m23(-1f)
-        projectionMatrix.m32(-((2 * FAR_PLANE * NEAR_PLANE) / viewDistance))
-        projectionMatrix.m33(0f)
+        projectionMatrix.identity()
+        
+        // We'll use a standard aspect ratio if none is provided, 
+        // but ideally this should come from the window
+        val aspectRatio = 1920f / 1080f 
+
+        if (isOrthographic) {
+            val left = -projectionSize.x / 2f
+            val right = projectionSize.x / 2f
+            val bottom = -projectionSize.y / 2f
+            val top = projectionSize.y / 2f
+            projectionMatrix.ortho(left, right, bottom, top, nearPlane, farPlane)
+        } else {
+            projectionMatrix.perspective(Math.toRadians(fov.toDouble()).toFloat(), aspectRatio, nearPlane, farPlane)
+        }
 
         return projectionMatrix
     }
@@ -56,11 +67,12 @@ class Camera(
     fun createViewMatrix(): Matrix4f {
         val viewMatrix = Matrix4f().identity()
 
-        viewMatrix.rotate(pitch.toRadians(), Vector3f(1f, 0f, 0f), viewMatrix)
-        viewMatrix.rotate(yaw.toRadians(), Vector3f(0f, 1f, 0f), viewMatrix)
-        viewMatrix.rotate(roll.toRadians(), Vector3f(0f, 0f, 1f), viewMatrix)
+        viewMatrix.rotate(pitch.toRadians(), Vector3f(1f, 0f, 0f))
+        viewMatrix.rotate(yaw.toRadians(), Vector3f(0f, 1f, 0f))
+        viewMatrix.rotate(roll.toRadians(), Vector3f(0f, 0f, 1f))
+        
         val negativeCameraPos = Vector3f(position).negate()
-        viewMatrix.translate(negativeCameraPos, viewMatrix)
+        viewMatrix.translate(negativeCameraPos)
 
         return viewMatrix
     }
