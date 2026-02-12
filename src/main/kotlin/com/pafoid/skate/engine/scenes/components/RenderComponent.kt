079f5839c1fa619bfde9755f1c0eff64ae6b084c
as00paf <as00paf@gmail.com>
2026-02-12 11:19:33 -0500

    Fix: Restore textures in RenderComponent.init() during cloning

--- src/main/kotlin/com/pafoid/skate/engine/scenes/components/RenderComponent.kt
+++ src/main/kotlin/com/pafoid/skate/engine/scenes/components/RenderComponent.kt
@@ -1,13 +1,10 @@
 package com.pafoid.skate.engine.scenes.components
 
+import com.pafoid.skate.engine.assets.ResourceManager
 import com.pafoid.skate.engine.models.BaseModel
+import com.pafoid.skate.engine.scenes.GameObject
 import kotlinx.serialization.Serializable
-
-enum class RenderMode {
-    MESH,
-    SKELETON,
-    BOTH
-}
+import org.koin.core.component.inject
 
 @Serializable
 class RenderComponent(
@@ -17,6 +14,20 @@ class RenderComponent(
     var textureScale: Float = 1.0f,
     var renderMode: RenderMode = RenderMode.MESH
 ) : Component() {
+
+    override fun init(gameObject: GameObject) {
+        super.init(gameObject)
+        val resourceManager: ResourceManager by inject()
+        
+        model.mesh.forEach { meshPart ->
+            val mat = meshPart.material
+            mat.baseColorPath?.let { mat.baseColorTexture = resourceManager.loadTextureSync(it) }
+            mat.normalMapPath?.let { mat.normalMap = resourceManager.loadTextureSync(it) }
+            mat.metallicRoughnessPath?.let { mat.metallicRoughnessTexture = resourceManager.loadTextureSync(it) }
+            mat.aoPath?.let { mat.aoTexture = resourceManager.loadTextureSync(it) }
+            mat.emissivePath?.let { mat.emissiveTexture = resourceManager.loadTextureSync(it) }
+        }
+    }
 
     override fun update(dt: Float) {
         // Render component doesn't need per-frame updates
