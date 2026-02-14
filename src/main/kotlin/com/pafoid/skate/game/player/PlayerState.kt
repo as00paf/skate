99d2af029294ffc78c72ea4fffcd93b310241562
Alexandre Fournier <as00paf@gmail.com>
2026-02-14 00:33:20 -0500

    Added new states

--- src/main/kotlin/com/pafoid/skate/game/player/PlayerState.kt
+++ src/main/kotlin/com/pafoid/skate/game/player/PlayerState.kt
@@ -3,6 +3,19 @@ package com.pafoid.skate.game.player
 sealed class PlayerState {
     object IDLE : PlayerState()
     object WALKING : PlayerState()
+    object RUNNING : PlayerState()
+    object JUMPING : PlayerState()
+    object FALLING : PlayerState()
+    object FALLING_IDLE : PlayerState()
+    object LAND : PlayerState()
+    object STRAFE_LEFT : PlayerState()
+    object STRAFE_RIGHT : PlayerState()
+    object WALK_STRAFE_LEFT : PlayerState()
+    object WALK_STRAFE_RIGHT : PlayerState()
+    object TURN_90_LEFT : PlayerState()
+    object TURN_90_RIGHT : PlayerState()
+    object TURN_180_LEFT : PlayerState()
+    object TURN_180_RIGHT : PlayerState()
 
 
     // Will come later with more
