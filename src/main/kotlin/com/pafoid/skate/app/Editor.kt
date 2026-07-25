158853919a867f112e699152b0e14e7d98be4440
as00paf <as00paf@gmail.com>
2026-07-25 16:58:53 -0400

    Cleanup Regrouped editor action handlers in one class

--- src/main/kotlin/com/pafoid/skate/app/Editor.kt
+++ src/main/kotlin/com/pafoid/skate/app/Editor.kt
@@ -9,12 +9,9 @@ import com.pafoid.skate.editor.systems.GizmoSystem
 import com.pafoid.skate.editor.systems.ProjectManager
 import com.pafoid.skate.editor.systems.SettingsManager
 import com.pafoid.skate.editor.systems.UndoRedoManager
-import com.pafoid.skate.editor.ui.handlers.ConsoleActionHandler
+import com.pafoid.skate.editor.ui.handlers.EditorActionHandler
 import com.pafoid.skate.editor.ui.handlers.EditorEventHandler
 import com.pafoid.skate.editor.ui.handlers.EditorInputHandler
-import com.pafoid.skate.editor.ui.handlers.EnvironmentActionHandler
-import com.pafoid.skate.editor.ui.handlers.ProjectActionHandler
-import com.pafoid.skate.editor.ui.handlers.SceneActionHandler
 import com.pafoid.skate.engine.core.Engine
 import com.pafoid.skate.engine.core.StringManager
 import com.pafoid.skate.engine.core.Window
@@ -36,11 +33,8 @@ class Editor(
     val editorInputHandler =
         EditorInputHandler(clipboardService, undoRedoManager, editorInputState, engine, settingsManager)
 
-    // TODO: regroup into one action handler
-    val sceneActionHandler = SceneActionHandler(engine, projectManager, undoRedoManager, mutationGate)
-    val projectActionHandler = ProjectActionHandler(engine, projectManager, undoRedoManager, stringManager)
-    val environmentActionHandler = EnvironmentActionHandler(undoRedoManager, engine.eventSystem)
-    val consoleActionHandler = ConsoleActionHandler(engine.eventSystem, engine.logger, undoRedoManager)
+    val editorActionHandler =
+        EditorActionHandler(engine, undoRedoManager, clipboardService, mutationGate, projectManager, stringManager)
 
     val editorCamera = EditorCamera(Camera().also { it.position.set(Vector3f(0f, 5f, 20f)) }, editorInputState)
     val gizmoSystem = GizmoSystem(engine, settingsManager, undoRedoManager, editorCamera)
