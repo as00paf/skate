c61f2c2c918813364021a5732a69c81973f486b7
as00paf <as00paf@gmail.com>
2026-08-04 15:38:55 -0400

    Cleanup Components

--- src/main/kotlin/com/pafoid/skate/engine/ecs/components/EnvironmentComponent.kt
+++ src/main/kotlin/com/pafoid/skate/engine/ecs/components/EnvironmentComponent.kt
@@ -7,28 +7,19 @@ import kotlinx.serialization.Serializable
 import org.joml.Vector3f
 
 @Serializable
-class EnvironmentComponent(
+data class EnvironmentComponent(
     @Contextual
     var skyColor: Vector3f = Vector3f(0.6f, 0.7f, 0.9f),
-
     @Contextual
     var skyTint: Vector3f = Vector3f(1.0f, 1.0f, 1.0f),
-
     var skyExposure: Float = 1.0f,
-
     var skyRotation: Float = 0.0f,
-
     var skyTexture: Texture? = null,
-
     @Contextual
     var fogColor: Vector3f = Vector3f(0.8f, 0.8f, 0.8f),
-
     var fogDensity: Float = 0.0f,
-
     var fogGradient: Float = 1.5f,
-
     var renderSky: Boolean = true,
-
     var renderFog: Boolean = true
 ) : Component() {
 
