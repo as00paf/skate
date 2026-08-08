596d4f470ad45bc98ef843a88ac747a828f8a871
as00paf <as00paf@gmail.com>
2026-08-08 17:47:51 -0400

    Cleanup ModelRenderer

--- src/main/kotlin/com/pafoid/skate/engine/render/renderer/ModelRenderer.kt
+++ src/main/kotlin/com/pafoid/skate/engine/render/renderer/ModelRenderer.kt
@@ -1,9 +1,7 @@
 package com.pafoid.skate.engine.render.renderer
 
-import com.pafoid.skate.engine.assets.Assets
 import com.pafoid.skate.engine.assets.AssetsManager
 import com.pafoid.skate.engine.assets.data.Shader
-import com.pafoid.skate.engine.assets.data.Texture
 import com.pafoid.skate.engine.assets.data.models.AlphaMode
 import com.pafoid.skate.engine.assets.data.models.MeshPart
 import com.pafoid.skate.engine.assets.data.models.animations.Bone
@@ -21,7 +19,7 @@ import com.pafoid.skate.engine.render.utils.unbindVAO
 import com.pafoid.skate.engine.render.utils.withBlendState
 import com.pafoid.skate.engine.render.utils.withCullFace
 import com.pafoid.skate.engine.render.utils.withDepthMask
-import com.pafoid.skate.engine.utils.ShaderConst
+import com.pafoid.skate.engine.utils.ShaderConst.Uniforms
 import com.pafoid.skate.engine.utils.TextureSlots
 import org.joml.Matrix4f
 import org.joml.Quaternionf
@@ -45,14 +43,12 @@ class ModelRenderer(
      */
     private fun bindTexture(
         slot: Int,
-        texture: Texture?,
+        textureId: Int,
         shader: Shader,
-        uniformName: String
     ) {
         GL13.glActiveTexture(GL13.GL_TEXTURE0 + slot)
-        val tex = texture ?: assetsManager.getBundledTexture(Assets.Bundled.DEFAULT_TEXTURE)
-        glBindTexture(GL_TEXTURE_2D, tex.texId)
-        shader.uploadInt(uniformName, slot)
+        glBindTexture(GL_TEXTURE_2D, textureId)
+        shader.uploadInt(Uniforms.BASE_COLOR_TEXTURE, slot)
     }
 
     /**
@@ -68,31 +64,31 @@ class ModelRenderer(
 
         // Bind all PBR texture maps and upload uniforms
         material.baseColorTexture?.texId?.let { bindTexture(TextureSlots.BASE_COLOR, it) }
-        shader.uploadInt(ShaderConst.Uniforms.BASE_COLOR_TEXTURE, TextureSlots.BASE_COLOR)
-        shader.uploadVec4f(ShaderConst.Uniforms.BASE_COLOR_FACTOR, material.baseColorFactor)
+        shader.uploadInt(Uniforms.BASE_COLOR_TEXTURE, TextureSlots.BASE_COLOR)
+        shader.uploadVec4f(Uniforms.BASE_COLOR_FACTOR, material.baseColorFactor)
 
         val hasNormal = material.normalMap != null
         material.normalMap?.texId?.let { bindTexture(TextureSlots.NORMAL, it) }
-        shader.uploadInt(ShaderConst.Uniforms.NORMAL_MAP, TextureSlots.NORMAL)
-        shader.uploadBoolean(ShaderConst.Uniforms.HAS_NORMAL_MAP, hasNormal)
+        shader.uploadInt(Uniforms.NORMAL_MAP, TextureSlots.NORMAL)
+        shader.uploadBoolean(Uniforms.HAS_NORMAL_MAP, hasNormal)
 
         val hasMR = material.metallicRoughnessTexture != null
         material.metallicRoughnessTexture?.texId?.let { bindTexture(TextureSlots.METALLIC_ROUGHNESS, it) }
-        shader.uploadInt(ShaderConst.Uniforms.METALLIC_ROUGHNESS_TEXTURE, TextureSlots.METALLIC_ROUGHNESS)
-        shader.uploadBoolean(ShaderConst.Uniforms.HAS_METALLIC_ROUGHNESS_TEXTURE, hasMR)
-        shader.uploadFloat(ShaderConst.Uniforms.METALLIC_FACTOR, material.metallicFactor)
-        shader.uploadFloat(ShaderConst.Uniforms.ROUGHNESS_FACTOR, material.roughnessFactor)
+        shader.uploadInt(Uniforms.METALLIC_ROUGHNESS_TEXTURE, TextureSlots.METALLIC_ROUGHNESS)
+        shader.uploadBoolean(Uniforms.HAS_METALLIC_ROUGHNESS_TEXTURE, hasMR)
+        shader.uploadFloat(Uniforms.METALLIC_FACTOR, material.metallicFactor)
+        shader.uploadFloat(Uniforms.ROUGHNESS_FACTOR, material.roughnessFactor)
 
         val hasAO = material.aoTexture != null
         material.aoTexture?.texId?.let { bindTexture(TextureSlots.AO, it) }
-        shader.uploadInt(ShaderConst.Uniforms.AO_TEXTURE, TextureSlots.AO)
-        shader.uploadBoolean(ShaderConst.Uniforms.HAS_AO_TEXTURE, hasAO)
+        shader.uploadInt(Uniforms.AO_TEXTURE, TextureSlots.AO)
+        shader.uploadBoolean(Uniforms.HAS_AO_TEXTURE, hasAO)
 
         val hasEmissive = material.emissiveTexture != null
         material.emissiveTexture?.texId?.let { bindTexture(TextureSlots.EMISSIVE, it) }
-        shader.uploadInt(ShaderConst.Uniforms.EMISSIVE_TEXTURE, TextureSlots.EMISSIVE)
-        shader.uploadBoolean(ShaderConst.Uniforms.HAS_EMISSIVE_TEXTURE, hasEmissive)
-        shader.uploadVec3f(ShaderConst.Uniforms.EMISSIVE_FACTOR, material.emissiveFactor)
+        shader.uploadInt(Uniforms.EMISSIVE_TEXTURE, TextureSlots.EMISSIVE)
+        shader.uploadBoolean(Uniforms.HAS_EMISSIVE_TEXTURE, hasEmissive)
+        shader.uploadVec3f(Uniforms.EMISSIVE_FACTOR, material.emissiveFactor)
 
         // Alpha mode
         val alphaInt = when (material.alphaMode) {
@@ -100,8 +96,8 @@ class ModelRenderer(
             AlphaMode.MASK -> 1
             AlphaMode.BLEND -> 2
         }
-        shader.uploadInt(ShaderConst.Uniforms.ALPHA_MODE, alphaInt)
-        shader.uploadFloat(ShaderConst.Uniforms.ALPHA_CUTOFF, material.alphaCutoff)
+        shader.uploadInt(Uniforms.ALPHA_MODE, alphaInt)
+        shader.uploadFloat(Uniforms.ALPHA_CUTOFF, material.alphaCutoff)
 
         // Configure render state
         withBlendState(alphaInt == 2) {
@@ -128,16 +124,14 @@ class ModelRenderer(
 
         part.vaoId.bindVAO(part.enabledAttributes)
 
-        val hasBaseColor = material.baseColorTexture != null
-        if (hasBaseColor) {
+        material.baseColorTexture?.let {
             bindTexture(
                 TextureSlots.BASE_COLOR,
-                material.baseColorTexture,
+                it.texId,
                 shader,
-                ShaderConst.Uniforms.BASE_COLOR_TEXTURE
             )
         }
-        shader.uploadBoolean("u_HasBaseColorTexture", hasBaseColor)
+        shader.uploadBoolean("u_HasBaseColorTexture", material.baseColorTexture != null)
 
         // Alpha mode
         val alphaInt = when (material.alphaMode) {
@@ -145,8 +139,8 @@ class ModelRenderer(
             AlphaMode.MASK -> 1
             AlphaMode.BLEND -> 2
         }
-        shader.uploadInt(ShaderConst.Uniforms.ALPHA_MODE, alphaInt)
-        shader.uploadFloat(ShaderConst.Uniforms.ALPHA_CUTOFF, material.alphaCutoff)
+        shader.uploadInt(Uniforms.ALPHA_MODE, alphaInt)
+        shader.uploadFloat(Uniforms.ALPHA_CUTOFF, material.alphaCutoff)
 
         withCullFace(!part.material.doubleSided) {
             GL11.glDrawElements(GL11.GL_TRIANGLES, part.vertexCount, GL11.GL_UNSIGNED_INT, 0)
@@ -172,14 +166,14 @@ class ModelRenderer(
         val textureScale = renderComponent.textureScale
 
         // Upload global uniforms for this object
-        defaultShader.uploadMat4f(ShaderConst.Uniforms.TRANSFORMATION_MATRIX, transformationMatrix)
-        defaultShader.uploadFloat(ShaderConst.Uniforms.TEXTURE_SCALE, textureScale)
-        defaultShader.uploadVec3f(ShaderConst.Uniforms.CAMERA_POSITION, cameraPosition)
+        defaultShader.uploadMat4f(Uniforms.TRANSFORMATION_MATRIX, transformationMatrix)
+        defaultShader.uploadFloat(Uniforms.TEXTURE_SCALE, textureScale)
+        defaultShader.uploadVec3f(Uniforms.CAMERA_POSITION, cameraPosition)
 
         val hasSkin = skeletonComponent?.pose != null
-        defaultShader.uploadBoolean(ShaderConst.Uniforms.HAS_SKIN, hasSkin)
+        defaultShader.uploadBoolean(Uniforms.HAS_SKIN, hasSkin)
         if (skeletonComponent != null) {
-            defaultShader.uploadMat4fArray(ShaderConst.Uniforms.JOINT_MATRICES, skeletonComponent.getMatrixPalette())
+            defaultShader.uploadMat4fArray(Uniforms.JOINT_MATRICES, skeletonComponent.getMatrixPalette())
         }
 
         val skeleton = skeletonComponent?.pose?.skeleton
@@ -212,13 +206,13 @@ class ModelRenderer(
         skeletonComponent: SkeletonComponent? = null
     ) {
         val transformationMatrix = transform.toWorldMatrix()
-        shader.uploadMat4f(ShaderConst.Uniforms.TRANSFORMATION_MATRIX, transformationMatrix)
-        shader.uploadFloat(ShaderConst.Uniforms.TEXTURE_SCALE, renderComponent.textureScale)
+        shader.uploadMat4f(Uniforms.TRANSFORMATION_MATRIX, transformationMatrix)
+        shader.uploadFloat(Uniforms.TEXTURE_SCALE, renderComponent.textureScale)
 
         val hasSkin = skeletonComponent?.pose != null
-        shader.uploadBoolean(ShaderConst.Uniforms.HAS_SKIN, hasSkin)
+        shader.uploadBoolean(Uniforms.HAS_SKIN, hasSkin)
         if (skeletonComponent?.pose != null) {
-            shader.uploadMat4fArray(ShaderConst.Uniforms.JOINT_MATRICES, skeletonComponent.getMatrixPalette())
+            shader.uploadMat4fArray(Uniforms.JOINT_MATRICES, skeletonComponent.getMatrixPalette())
         }
 
         val skeleton = skeletonComponent?.pose?.skeleton
